27863c601e40b2ebed3e56644643c6d8baf9aeca
Rio Foster <friotious@gmail.com>
2022-05-18 10:02:26 -0700

    Remove comments

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
     .then(response => {
       if(response.ok){
-        //console.log(response, 'response')
+   
         return response.json();
       } else {
         throw Error(response.status);
@@ -35,59 +35,7 @@ class App extends Component {
             movieID: id })
   }
 
-  // findMovie = (id) => {
-  //   //iterate over state all movies and see if we can find matching ids, return that id, which
-  //   console.log(id)
-  //   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  //   .then(response => {
-  //     if(response.ok) {
-  //       return response.json();
-  //     } else {
-  //       throw Error(response.status);
-  //     }})
-  //     .then(movie => {
-  //       this.setState({ movies: [...this.state.movies],
-  //       movieSelected: movie })
-  //       console.log(this.state.movieSelected)
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //       this.setState({ movies: [], movieSelected: { },
-  //       status: true })})
-  // }
-
-  // async findMovie = (id) => {
-  //   await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  //   .then(response => {
-  //     if(response.ok) {
-  //       return response.json();
-  //     } else {
-  //       throw Error(response.status);
-  //     }})
-  //     .then(movie => {
-  //       return movie
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     })
-  // }
-
-
-  // findMovie(id) {
-  //   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  //   .then(response => {
-  //     if(response.ok) {
-  //       return response.json();
-  //     } else {
-  //       throw Error(response.status);
-  //     }})
-  //     .then(movie => {
-  //       return movie
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     })
-  // }
+  
 
   redirectHome = () => {
     this.setState({ movies: [...this.state.movies],
@@ -108,12 +56,4 @@ class App extends Component {
 };
 
 export default App;
-
-// change details container to a class comonent, move fetch to componentDidMount and then we would match the id from the params to the id of the component, and we could render accordingly
-
-
-        // {this.state.status && <h2 className='error-message'>Oops, something went wrong</h2>}
-
-// {this.state.movieSelected ?
-//   <MovieDetailsContainer movieSelected={ this.state.movieSelected } redirectHome={ this.redirectHome} status={this.state.status}/> : <MoviesContainer movies={this.state.movies} findMovie={ this.findMovie }/> }
 
--- src/components/MovieDetailsContainer.js
+++ src/components/MovieDetailsContainer.js
@@ -3,41 +3,21 @@ import '../css/MovieDetailsContainer.css';
 import MovieDetailsCard from './MovieDetailsCard';
 
 
-// const MoviesDetailsContainer = ({ movieSelected, redirectHome, status }) => {
-//   if (status) {
-//     return;
-//   } else {
-//   return (
-//     <div className="MoviesDetailsContainer" style={{
-//           backgroundImage: `url(${movieSelected.movie.backdrop_path})`
-//         , backgroundSize: "100% auto"}}>
-//       <MovieDetailsCard posterUrl={ movieSelected.movie.poster_path }
-//                         title={movieSelected.movie.title}
-//                         averageRating={movieSelected.movie.average_rating}
-//                         id={movieSelected.movie.id}
-//                         runtime={movieSelected.movie.runtime}
-//                         overview={movieSelected.movie.overview}/>
-//       <button className="take-me-home" onClick={() => redirectHome()}>TAKE ME HOME</button>
-//     </div>);
-//   }
-// };
-
 class MoviesDetailsContainer extends Component {
   constructor({id, redirectHome}) {
-    //console.log(id, 'id fuckoff')
+
     super()
     this.state = {
       testID: id,
       test: 'Fuck'
     }
-    //this.showMovie(id)
-    //console.log(this.state, 'state??')
+  
   }
 
   showMovie = (id) => {
     if (this.state.test === 'Fuck') {fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.testID}`)
     .then(response => {
-      //console.log(response, 'respnse from moviedetailscontainer')
+      
       if(response.ok) {
         return response.json();
       } else {
@@ -81,46 +61,6 @@ class MoviesDetailsContainer extends Component {
         status: true })})
   }}
 
-  // componentDidUpdate = () => {
-  //   if (this.state.test === 'Fuck') {fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.testID}`)
-  //   .then(response => {
-  //     console.log(response, 'respnse from UPDATE')
-  //     if(response.ok) {
-  //       return response.json();
-  //     } else {
-  //       throw Error(response.status);
-  //     }})
-  //     .then(movie => {
-  //       console.log(movie, 'movie in UPDATE')
-  //       this.setState({ 
-  //           movieSelected: movie,
-  //           test: 'yay'
-  //        })
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //       this.setState({ movieSelected: { },
-  //       status: true })})
-  // }}
-  // <>
-  //       <h1>Fuck This!!</h1>
-  //     </>
-
-  // showMovie = (redirectHome) => {
-  //   return(
-  //       <div className="MoviesDetailsContainer" style={{
-  //                 backgroundImage: `url(${this.state.movieSelected.backdrop_path})`
-  //               , backgroundSize: "100% auto"}}>
-  //             <MovieDetailsCard posterUrl={ this.state.movieSelected.movie.poster_path }
-  //                               title={this.state.movieSelected.movie.title}
-  //                               averageRating={this.state.movieSelected.movie.average_rating}
-  //                               id={this.state.movieSelected.movie.id}
-  //                               runtime={this.state.movieSelected.movie.runtime}
-  //                               overview={this.state.movieSelected.movie.overview}/>
-  //             <button className="take-me-home" onClick={() => redirectHome()}>TAKE ME HOME</button>
-  //           </div>
-  //     )
-  // }
 
 
 showPoop = (redirectHome) => {
@@ -139,8 +79,7 @@ showPoop = (redirectHome) => {
   )
 }
 
-// this.showMovie()
-// if (this.state)
+
 
   render(redirectHome) {
     if (this.state.test === "Fuck") {
@@ -169,16 +108,3 @@ showPoop = (redirectHome) => {
 
 export default MoviesDetailsContainer;
 
-// return(
-//   <div className="MoviesDetailsContainer" style={{
-//             backgroundImage: `url(${this.state.movieSelected.backdrop_path})`
-//           , backgroundSize: "100% auto"}}>
-//         <MovieDetailsCard posterUrl={ this.state.movieSelected.movie.poster_path }
-//                           title={this.state.movieSelected.movie.title}
-//                           averageRating={this.state.movieSelected.movie.average_rating}
-//                           id={this.state.movieSelected.movie.id}
-//                           runtime={this.state.movieSelected.movie.runtime}
-//                           overview={this.state.movieSelected.movie.overview}/>
-//         <button className="take-me-home" onClick={() => redirectHome()}>TAKE ME HOME</button>
-//       </div>
-// )
